b5ad5e8d2a0bc5d4b5bab098e745dac2a1417231
NeethaChacko <neetha.chacko@gmail.com>
2021-06-20 02:20:53 +0200

    Three separate files

--- add_to_cart_test.py
+++ add_to_cart_test.py
@@ -1,8 +1,22 @@
 import unittest
+from dataclasses import dataclass
+from typing import List
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+@dataclass
+class Cart:
+    items: List
+
+    @staticmethod
+    def calculate_total_price():
+        return 0
+
+
+class AddToCartTestCase(unittest.TestCase):
+    def test_empty_cart(self):
+        cart = Cart([])
+        self.assertEqual(cart.calculate_total_price(), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
